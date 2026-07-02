0f2392a02ec7562fe3ed3996ea74501f3359e216
berranuralinca <berranuralinca1108@gmail.com>
2026-07-02 23:12:30 +0300

    CSS modules

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
-    <div>
-      <AddToCart />
-    </div>
+    <div className={styles.card}>
+        <AddToCart />
+      </div>
   )
 }
 
